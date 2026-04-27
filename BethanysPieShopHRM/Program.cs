987e0594c7a1eb84cff736f0168539f4b4f0f544
NiklasSod <nma.prob@gmail.com>
2026-04-27 17:43:59 +0200

    include projects

---
+++ BethanysPieShopHRM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BethanysPieShopHRM.Utilities;
+
+namespace BethanysPieShopHRM
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bethany's Pie Shop");
+
+            MyFunctions.SayHello("Niklas");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BethanysPieShopHRM/Utilities/MyFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopHRM.Utilities
+{
+    internal class MyFunctions
+    {
+        public static void SayHello(string name)
+        {
+            Console.WriteLine($"Hej {name} från Utilities-mappen!");
+        }
+    }
+}
