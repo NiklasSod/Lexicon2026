48516551a64e415f9321f8dc62eb1e02a6bb0f73
NiklasSod <nma.prob@gmail.com>
2026-04-28 12:14:19 +0200

    starting exercise 1

---
+++ Lexicon_Core/Exercise_01/PersonalRegister.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * Vilka klasser bör ingå i programmet?
+ * 
+ * En känns rimligt, allt handlar om liknande sak.
+ * 
+ * 
+ * Vilka attribut och metoder bör ingå i dessa klasser?
+ * 
+ * Tänker mig en Main och två hjälpklasser. Add / Read
+ */
+
+namespace Lexicon2026.Exercise_01
+{
+    internal class PersonalRegister
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Restaurant Restaurontos personal register.");
+            while (true) // console is not closed
+            {
+                Console.WriteLine("Add a new person to the crew? Press 1");
+                Console.WriteLine("Look up workers? Press 2");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Lexicon_Core/Program.cs
+++ Lexicon_Core/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Lexicon2026.Exercise_01;
+//using Lexicon2026.Exercise_02;
+//using Lexicon2026.Exercise_03;
+
+PersonalRegister.Main(); // Exercise_01
+//FutureExercise02();
+//FutureExercise03();
