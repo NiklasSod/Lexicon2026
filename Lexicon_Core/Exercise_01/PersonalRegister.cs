db4743b1f640889495df4c216c249b104bad1231
NiklasSod <nma.prob@gmail.com>
2026-04-28 14:07:06 +0200

    work on user input

--- Lexicon_Core/Exercise_01/PersonalRegister.cs
+++ Lexicon_Core/Exercise_01/PersonalRegister.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 /*
@@ -10,7 +11,7 @@ using System.Text;
  * 
  * Vilka attribut och metoder bör ingå i dessa klasser?
  * 
- * Tänker mig en Main och två hjälpklasser. Add / Read
+ * Tänker mig en Main och två hjälpklasser. Add / Read - edit: en till som kontrollerar user input.
  */
 
 namespace Lexicon2026.Exercise_01
@@ -20,12 +21,55 @@ namespace Lexicon2026.Exercise_01
         public static void Main()
         {
             Console.WriteLine("Welcome to Restaurant Restaurontos personal register.");
-            while (true) // console is not closed
+
+            // add list
+
+            while (true) // console is not closed by user
             {
-                Console.WriteLine("Add a new person to the crew? Press 1");
-                Console.WriteLine("Look up workers? Press 2");
-                Console.ReadLine();
+                Console.WriteLine("Press 1 to add a new person to the crew? ");
+                Console.WriteLine("Press 2 to look up workers?");
+                Console.WriteLine();
+
+                int userChoice = UserInput(Console.ReadLine());
+
+                switch(userChoice)
+                {
+                    case 1:
+                        Console.WriteLine("What is the name of the person?");
+                        string? inputName = Console.ReadLine();
+                        while (string.IsNullOrWhiteSpace(inputName))
+                        {
+                            Console.WriteLine("Name cannot be empty, enter name again:");
+                            inputName = Console.ReadLine();
+                        }
+
+                        Console.WriteLine("What salary will they recieve?");
+                        string? inputSalary = Console.ReadLine();
+                        int salary = UserInput(inputSalary);
+                        while (salary <= 0)
+                        {
+                            Console.WriteLine("Incorrect, enter salary again:");
+                            salary = UserInput(Console.ReadLine());
+                        };
+                        Console.WriteLine("New employee added!");
+                        Console.WriteLine();
+
+                        // add to list { name, salary }
+                        break;
+                    case 2:
+                        // todo show table
+                    default:
+                        Console.WriteLine("Invalid input. Please enter 1 or 2.");
+                        break;
+                }
             }
+        }
+
+        public static int UserInput(string? input)
+        {
+            if (input is null) return 0;
+            if (!int.TryParse(input, out int userChoice)) return 0;
+            return int.Parse(input);
         }
     }
 }
