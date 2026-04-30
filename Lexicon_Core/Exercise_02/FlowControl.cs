f626320c33703c99cef24460a87c5337338695ad
NiklasSod <nma.prob@gmail.com>
2026-04-30 14:06:03 +0200

    exercise 2 init

---
+++ Lexicon_Core/Exercise_02/FlowControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexicon2026.Exercise_02
+{
+    internal class FlowControl
+    {
+        public static void Main()
+        {
+            // user menu
+
+            // wrap the rest in in while(true)
+
+            // switch case with 2 cases (for now)
+            // 0 to close program
+            // default for handling incorrect inputs
+        }
+
+        // other methods are ok like UserMenu()
+    }
+}
