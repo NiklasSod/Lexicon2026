7eefa2c3c89467f658fffe41b15131ac667d7009
NiklasSod <nma.prob@gmail.com>
2026-05-06 21:15:37 +0200

    add Person class and main method test for it

---
+++ Lexicon_Core/Exercise_03/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexicon2026.Exercise_03
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+
+        public string FirstName => _firstName;
+        public int Age => _age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{_firstName} {_lastName} is {_age} years old.";
+        }
+    }
+}
--- Lexicon_Core/Program.cs
+++ Lexicon_Core/Program.cs
@@ -1,7 +1,7 @@
 ﻿//using Lexicon2026.Exercise_01;
-using Lexicon2026.Exercise_02;
-//using Lexicon2026.Exercise_03;
+//using Lexicon2026.Exercise_02;
+using Lexicon2026.Exercise_03;
 
 //PersonalRegister.Main(); // Exercise_01
-FlowControl.Main(); // Exercise02();
-//FutureExercise03();
+//FlowControl.Main(); // Exercise02();
+Encapsulation.Main();
