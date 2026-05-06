f7bc3f6d23acd10027727ade446f4b5d78921625
NiklasSod <nma.prob@gmail.com>
2026-05-06 21:30:56 +0200

    add 2nd test

--- Lexicon_Core/Exercise_03/Person.cs
+++ Lexicon_Core/Exercise_03/Person.cs
@@ -9,20 +9,34 @@ namespace Lexicon2026.Exercise_03
         private string _firstName;
         private string _lastName;
         private int _age;
+        private decimal _salary;
 
         public string FirstName => _firstName;
         public int Age => _age;
 
-        public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             _firstName = firstName;
             _lastName = lastName;
             _age = age;
+            _salary = salary;
         }
 
+        // test 1
+        //public override string ToString()
+        //{
+        //    return $"{_firstName} {_lastName} is {_age} years old.";
+        //}
+
+        // test 2
         public override string ToString()
         {
-            return $"{_firstName} {_lastName} is {_age} years old.";
+            return $"{_firstName} {_lastName} receives {_salary} dollars.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            _salary *= (1 + percentage / 100);
         }
     }
 }
