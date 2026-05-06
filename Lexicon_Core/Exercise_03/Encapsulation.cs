9aee7167686ff849cab1140b9300c19e48517f3b
NiklasSod <nma.prob@gmail.com>
2026-05-06 22:04:57 +0200

    add try catch

--- Lexicon_Core/Exercise_03/Encapsulation.cs
+++ Lexicon_Core/Exercise_03/Encapsulation.cs
@@ -31,12 +31,18 @@ namespace Lexicon2026.Exercise_03
             var persons = new List<Person>();
             for (int i = 0; i < lines; i++)
             {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0],
-                cmdArgs[1],
-               int.Parse(cmdArgs[2]),
-               decimal.Parse(cmdArgs[3]));
-                persons.Add(person);
+                try
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2]),
+                    decimal.Parse(cmdArgs[3]));
+                    persons.Add(person);
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
             var bonus = decimal.Parse(Console.ReadLine());
             persons.ForEach(p => p.IncreaseSalary(bonus));
