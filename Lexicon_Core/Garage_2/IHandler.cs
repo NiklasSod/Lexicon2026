a5ea83b28e4eeafc87108109f04385f760a7fc80
NiklasSod <nma.prob@gmail.com>
2026-05-20 01:45:27 +0200

    add IHandler skeleton

---
+++ Lexicon_Core/Garage_2/IHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexicon2026.Garage_2
+{
+    public interface IHandler
+    {
+        //// Setup
+        //void CreateGarage(int capacity);
+        //void SeedDefaultVehicles();
+
+        //// Core actions
+        //string ParkVehicle(Vehicle vehicle);
+        //string RemoveVehicle(string registrationNumber);
+
+        //// List and Filter actions
+        //IEnumerable<string> ListAllVehicles();
+        //IEnumerable<string> FilterByVehicleType(int typeKey);
+        //IEnumerable<string> FilterByAttributes(int amount, string attributeKey);
+    }
+}
