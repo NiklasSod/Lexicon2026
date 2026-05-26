63bc7175a3a9ffd27d7efd64e30203ddf054be9b
NiklasSod <nma.prob@gmail.com>
2026-05-26 23:43:23 +0200

    add exercise2 for delegates

---
+++ Lexicon_Core/Exercise_08/Exercise_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexicon2026.Exercise_08.Exercise_2
+{
+    public delegate string WriteBack(string name);
+
+    class Program
+    {
+        public static void Main()
+        {
+            WriteBack myDelegate = Hello;
+            myDelegate = Friendly;
+            myDelegate = Goodbye;
+            string result1 = myDelegate("Niklas");
+            Console.WriteLine(result1);
+
+            WriteBack multiDelegate = Hello;
+            multiDelegate += Friendly;
+            multiDelegate += Goodbye;
+            string multiResult = multiDelegate("Niklas");
+            Console.WriteLine(multiResult); // sista körs
+
+            Console.ReadLine();
+        }
+
+        public static string Hello(string name) => $"Hi {name}.\n";
+        public static string Friendly(string name) => $"Hope all is well, {name}\n";
+        public static string Goodbye(string name) => $"Goodbye {name}!\n";
+    }
+}
