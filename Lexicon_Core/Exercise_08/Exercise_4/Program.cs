8f8954f50195148c9f52ed6976fc5c1662fc24c2
NiklasSod <nma.prob@gmail.com>
2026-05-27 15:29:51 +0200

    exercise4 done

--- Lexicon_Core/Exercise_08/Exercise_4/Program.cs
+++ Lexicon_Core/Exercise_08/Exercise_4/Program.cs
@@ -10,7 +10,14 @@ namespace Lexicon2026.Exercise_08.Exercise_4
 
         public static void Main()
         {
+            MessageDel myDel = DelOne;
+            myDel += DelTwo;
+            myDel("Test från Main!");
 
+            // The Event and Delegate connection:
+            // Delegaten är ritning och behållare. Inget Event utan att först ha en Delegate.
+            // Här finns inget Event, med Event kan andra klasser endast välja att 
+            // += prenumerera eller -= avprenumerera.
         }
 
         public static void DelOne(string message)
