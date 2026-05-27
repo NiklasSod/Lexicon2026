fac25908325f4421089b552a3346e7b3f02a30c5
NiklasSod <nma.prob@gmail.com>
2026-05-27 15:18:20 +0200

    create a delegate

---
+++ Lexicon_Core/Exercise_08/Exercise_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexicon2026.Exercise_08.Exercise_4
+{
+    internal class Program
+    {
+        public delegate void MessageDel(string message);
+        public static void Main()
+        {
+
+        }
+    }
+}
