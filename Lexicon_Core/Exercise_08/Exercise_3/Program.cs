476c9faace83f9e04282f6dd350d8e508f315ebb
NiklasSod <nma.prob@gmail.com>
2026-05-27 15:10:12 +0200

    add subscriber

---
+++ Lexicon_Core/Exercise_08/Exercise_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexicon2026.Exercise_08.Exercise_3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Publisher pub = new Publisher();
+            Subscriber sub = new Subscriber();
+
+            // Connecting sub to event through delegate
+            pub.ProcessCompleted += sub.OnProcessCompleted;
+
+            Console.WriteLine("--- Startar ---");
+            pub.StartProcess(true);
+
+            Console.WriteLine();
+            // pub.ProcessCompleted -= sub.OnProcessCompleted;
+        }
+    }
+}
---
+++ Lexicon_Core/Exercise_08/Exercise_3/Subscriber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexicon2026.Exercise_08.Exercise_3
+{
+    internal class Subscriber
+    {
+        public void OnProcessCompleted(object source, EventArgs args)
+        {
+            Console.WriteLine("Subscriber körs!");
+        }
+    }
+}
