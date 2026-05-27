ca44a62cf2a328c18aabf37badce9e0715f0017b
NiklasSod <nma.prob@gmail.com>
2026-05-27 15:20:43 +0200

    add two methods for delegate

--- Lexicon_Core/Exercise_08/Exercise_4/Program.cs
+++ Lexicon_Core/Exercise_08/Exercise_4/Program.cs
@@ -7,9 +7,20 @@ namespace Lexicon2026.Exercise_08.Exercise_4
     internal class Program
     {
         public delegate void MessageDel(string message);
+
         public static void Main()
         {
 
+        }
+
+        public static void DelOne(string message)
+        {
+            Console.WriteLine($"One + {message}");
+        }
+
+        public static void DelTwo(string message)
+        {
+            Console.WriteLine($"Two + {message}");
         }
     }
 }
